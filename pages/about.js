1d79a7d0c84ce9a1ac6976024cc7b42fe2d812f4
azocher <anna.zocher@gmail.com>
2020-08-07 16:16:07 -0700

    added random testing pages for demo

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/navbar'
+
+export default function About() {
+    return (
+        <div>
+            <Navbar />
+            <div>
+                <h1>👋 Hi! My Name is Anna.</h1>
+            </div>
+        </div>
+    )
+}
